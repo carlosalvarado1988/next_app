ca0ef16ea9e77a14ff2d1b8549833f4adda84bbc
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-10-08 23:08:46 -0600

    programatic navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UsersTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -8,6 +9,9 @@ const UsersPage = ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users:</h1>
+      <Link href={"/users/new"} className="btn mb-5">
+        New User
+      </Link>
       <UsersTable sortOrder={sortOrder} />
     </>
   );
