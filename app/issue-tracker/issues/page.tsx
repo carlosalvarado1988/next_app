ff7159ddbf697a6ddfa94b63edd2bf2f38d73d55
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-10-13 10:57:35 -0600

    implementing view issues table

--- app/issue-tracker/issues/page.tsx
+++ app/issue-tracker/issues/page.tsx
@@ -1,20 +1,49 @@
-"use client";
-
 import React from "react";
-import { Button } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { Button, Table } from "@radix-ui/themes";
+// import { useRouter } from "next/navigation";
+import prisma from "@/prisma/client";
+import Link from "next/link";
 
-const IssuesPage = () => {
-  const router = useRouter();
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany({});
+
+  // const router = useRouter();
   return (
     <div>
-      <Button
-        onClick={() => {
-          router.push("/issue-tracker/issues/new");
-        }}
-      >
-        New Issue
-      </Button>
+      <div className="mb-3">
+        <Link href={"/issue-tracker/issues/new"}>
+          <Button>New Issue</Button>
+        </Link>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
